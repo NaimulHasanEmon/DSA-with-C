ed2922355c93685a98f27fdc56a5c5207a33bc17
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-08-06 11:30:45 +0600

    Update and rename Main.c to Linked List creation and traverse using recursion.c

--- Linked List creation and traverse using recursion.c
+++ Linked List creation and traverse using recursion.c
@@ -5,7 +5,20 @@ struct Node
     int data;
     struct Node *next;
 };
-
+void LinkedListTraversal(struct Node *ptr)
+{
+    if (ptr == NULL)
+    {
+        return;
+    }
+    printf("%d", ptr->data);
+    ptr = ptr->next;
+    if (ptr != NULL)
+    {
+        printf(" -> ");
+    }
+    LinkedListTraversal(ptr);
+}
 int main()
 {
     struct Node *head, *second, *third, *fourth;
@@ -13,25 +26,14 @@ int main()
     second = (struct Node *)malloc(sizeof(struct Node));
     third = (struct Node *)malloc(sizeof(struct Node));
     fourth = (struct Node *)malloc(sizeof(struct Node));
-    head->data = 1;
+    head->data = 7;
     head->next = second;
-    second->data = 2;
+    second->data = 45;
     second->next = third;
-    third->data = 3;
+    third->data = 69;
     third->next = fourth;
-    fourth->data = 4;
+    fourth->data = 100;
     fourth->next = NULL;
-    struct Node *currNode = head;
-    while (currNode != NULL)
-    {
-        printf("%d -> ", currNode->data);
-        currNode = currNode->next;
-        if (currNode == NULL)
-            printf("NULL");
-    }
-    free(head);
-    free(second);
-    free(third);
-    free(fourth);
+    LinkedListTraversal(head);
     return 0;
 }

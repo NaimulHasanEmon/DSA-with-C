a233bc5c8764012e5285802512b62376315a9f39
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-08-10 11:35:39 +0600

    Add files via upload

---
+++ Main.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+// Merge sort algorithm
+// Time complexity O(nlogn)
+
+void conquer(int arr[], int si, int mid, int ei)
+{
+    int merged[ei - si + 1];
+    int idx1 = si, idx2 = mid + 1, x = 0;
+    while (idx1 <= mid && idx2 <= ei)
+    {
+        if (arr[idx1] <= arr[idx2])
+            merged[x++] = arr[idx1++];
+        else
+            merged[x++] = arr[idx2++];
+    }
+    while (idx1 <= mid)
+    {
+        merged[x++] = arr[idx1++];
+    }
+    while (idx2 <= ei)
+    {
+        merged[x++] = arr[idx2++];
+    }
+    for (int i = 0; i < ei - si + 1; i++)
+    {
+        arr[si + i] = merged[i];
+    }
+}
+void divide(int arr[], int si, int ei)
+{
+    if (si >= ei)
+        return;
+    int mid = si + (ei - si) / 2;
+    divide(arr, si, mid);
+    divide(arr, mid + 1, ei);
+    conquer(arr, si, mid, ei);
+}
+int main()
+{
+    printf("How many elements you want to add?: ");
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter array elements: ");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    printf("Original array: ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    divide(arr, 0, n - 1);
+    printf("Original array: ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}

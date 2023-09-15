80f7c1e16965fd089046ba7e299e0be29980b3a6
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-09-15 12:22:14 +0600

    Add files via upload

---
+++ new.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct Node
+{
+    int data;
+    struct Node *next;
+} node;
+
+struct Node *top;
+int size = 0;
+
+void push(int data)
+{
+    node *newNode = (node *)malloc(sizeof(node));
+    newNode->data = data;
+    newNode->next = top;
+    top = newNode;
+    size++;
+}
+
+void pop()
+{
+    if (top == NULL)
+    {
+        printf("Stack is empty.\n");
+        return;
+    }
+    node *currNode = top;
+    top = top->next;
+    free(currNode);
+    currNode = NULL;
+    size--;
+}
+
+void sizeOfList()
+{
+    printf("List size is: %d\n", size);
+}
+
+void printList(node *top)
+{
+    node *tempNode = top;
+    if (top == NULL)
+    {
+        printf("Stack is empty.\n");
+        return;
+    }
+    printf("List is: ");
+    while (tempNode != NULL)
+    {
+        printf("%d ", tempNode->data);
+        tempNode = tempNode->next;
+    }
+    printf("\n");
+}
+
+int main()
+{
+    printf("How many elements you want to add: ");
+    int n, data;
+    scanf("%d", &n);
+    printf("Enter elements: ");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &data);
+        push(data);
+    }
+    printList(top);
+    sizeOfList();
+    printf("How many elements you want to pop: ");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        pop();
+        printList(top);
+        sizeOfList();
+    }
+    return 0;
+}
